753353771cdec8a3093862610479bbaa1b620b5f
markoka101 <markoka101@gmail.com>
2025-05-16 14:42:00 -0400

    clarified comments for those with little to no background in code

--- index.js
+++ index.js
@@ -1,74 +1,81 @@
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+//functions for alert and email functionality taken from respective files
 const sendEmail = require("./sendEmail.js");
 const noti = require("./window-notify.js");
+
+//variables for the page
 const { path, htmlSection } = require("./PageVariables.js");
 
 // Add stealth plugin
 puppeteer.use(StealthPlugin());
 
 /*
-This kinda simulates a human clicking the refresh button and uses random timing
+This kinda simulates a human by openning an instance of the webpage and refreshing using random timing
+Times can easily be adjusted, but I wouldn't recommend making it shorter
 */
 (async () => {
     //launching broswer and starting page
-    //if you want to see the browser set headleess to false, but it will take more resources
+    //if you want to see the browser set headleess to false, but it will take more resources and doesn't change anything
     const browser = await puppeteer.launch({
         headless: true,
     });
     const page = await browser.newPage();
 
-    //go to the page
+    //go to the specified path
     await page.goto(path);
 
-    //runs until the button does not exisit
+    //runs until the button does not exist
     while (true) {
-        //this is the button
+        //the button is set for the specified section in the page
         const buttonSelector = htmlSection;
 
         //track whether the button was found
         let found = false;
 
-        //run 8 times to make sure there is no fluke
+        //this will check for the button. It will run 8 times to make sure there is no false reading
         for (let i = 0; i < 8; i++) {
             const button = await page.$(buttonSelector);
+            //checks if the button does in fact exist, if it does we break this for loop
             if (button) {
-                //extract text from button
+                //extract text from button and trim leading and tailing whitespace
                 const buttonText = await page.evaluate(
                     (el) => el.textContent.trim(),
                     button
                 );
 
-                //only say button was found when it equals this text
+                //only say button was found when it equals this text as that means
+                //that the product is still not available
                 if (buttonText === "NOTIFY ME WHEN AVAILABLE") {
                     found = true;
                 }
 
                 break;
             } else {
+                //reload page then pause for 3 to 7 seconds
                 await page.reload();
 
-                // pause for 3 - 7 seconds and check again
                 await new Promise(
                     (res) => setTimeout(res, Math.random() * 7001) + 3000
                 );
             }
         }
 
-        //if after checks button isn't found send alerts and break out of loop
+        //if after checks button isn't found send notification and email then break out of loop
         if (!found) {
             noti.windowNotif();
             sendEmail.sendEmail();
             break;
         }
 
-        // pause for 30 - 60 seconds
+        // pause for 30 - 60 seconds then reload page
         await new Promise((res) =>
             setTimeout(res, Math.floor(Math.random() * 60001) + 30000)
         );
         await page.reload();
     }
 
-    //close browser and kill process
+    //close browser and complete program
     await browser.close();
 })();
